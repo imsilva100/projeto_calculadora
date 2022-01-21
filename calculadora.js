32b2a8519d5107033d1418fba8c3c7009663e53f
imsilva100 <ismael@sonner.com.br>
2022-01-21 08:21:13 -0300

    correção nos botões

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,56 @@
-var BotaoDigitado = document.querySelector(".teclas");
+var botaoDigitado = 0;
+botaoDigitado = document.querySelector("#botao_9");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 9;
+    console.log(digito);
+});
 
-BotaoDigitado.addEventListener("click", ObtemDigito);
+botaoDigitado = document.querySelector("#botao_8");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 8;
+    console.log(digito);
+});
 
-function ObtemDigito(botao){
-    var digito = document.getElementById(".botao");
-    console.log(digito); 
-}
+botaoDigitado = document.querySelector("#botao_7");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 7;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_6");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 6;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_5");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 5;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_4");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 4;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_3");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 3;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_2");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 2;
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#botao_1");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 1;
+    console.log(digito);
+});
+
+
