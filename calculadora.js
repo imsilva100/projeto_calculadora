286a5f23e61679cdbe366ff8cbf7d1f57071e291
imsilva100 <ismael@sonner.com.br>
2022-01-23 21:36:50 -0300

    guardando o display em um array

--- calculadora.js
+++ calculadora.js
@@ -2,55 +2,133 @@ var botaoDigitado = 0;
 botaoDigitado = document.querySelector("#botao_9");
 botaoDigitado.addEventListener("click", function (){
     var digito = 9;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_8");
 botaoDigitado.addEventListener("click", function (){
     var digito = 8;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_7");
 botaoDigitado.addEventListener("click", function (){
     var digito = 7;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_6");
 botaoDigitado.addEventListener("click", function (){
     var digito = 6;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_5");
 botaoDigitado.addEventListener("click", function (){
     var digito = 5;
+    atualizaDisplay(digito);
     console.log(digito);
+
 });
 
 botaoDigitado = document.querySelector("#botao_4");
 botaoDigitado.addEventListener("click", function (){
     var digito = 4;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_3");
 botaoDigitado.addEventListener("click", function (){
     var digito = 3;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_2");
 botaoDigitado.addEventListener("click", function (){
     var digito = 2;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#botao_1");
 botaoDigitado.addEventListener("click", function (){
     var digito = 1;
+    atualizaDisplay(digito);
     console.log(digito);
 });
 
+botaoDigitado = document.querySelector("#zero");
+botaoDigitado.addEventListener("click", function (){
+    var digito = 0;
+    atualizaDisplay(digito);
+    console.log(digito);
+});
 
+botaoDigitado = document.querySelector("#ponto");
+botaoDigitado.addEventListener("click", function (){
+    var digito = ".";
+    atualizaDisplay(digito);
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#divisao");
+botaoDigitado.addEventListener("click", function (){
+    var digito = "/";
+    atualizaDisplay(digito);
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#multiplicacao");
+botaoDigitado.addEventListener("click", function (){
+    var digito = "*";
+    atualizaDisplay(digito);
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#subtracao");
+botaoDigitado.addEventListener("click", function (){
+    var digito = "-";
+    atualizaDisplay(digito);
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#soma");
+botaoDigitado.addEventListener("click", function (){
+    var digito = "+";
+    atualizaDisplay(digito);
+    console.log(digito);
+});
+
+botaoDigitado = document.querySelector("#igual");
+botaoDigitado.addEventListener("click", calcula);
+
+botaoDigitado = document.querySelector("#limpa");
+botaoDigitado.addEventListener("click", limpaDisplay);
+
+
+function atualizaDisplay(valor){
+
+    var display = document.getElementById("display");
+    display.textContent = display.textContent + valor;
+    console.log(display);
+
+}
+
+function limpaDisplay(){
+    var display = document.getElementById("display");
+    display.textContent = "";
+    console.log(display);
+}
+
+function calcula(){
+    var calculo = document.getElementById("display");
+    calculo = calculo.textContent;
+    expressao = calculo.split("");
+    console.log(expressao);
+}
