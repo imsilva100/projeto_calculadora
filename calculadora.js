74d8d22cf7b35fcffa37d9befc6fffe3ae60f210
imsilva100 <ismael@sonner.com.br>
2022-02-07 18:31:47 -0300

    expressao com mais de 3 numero ok

--- calculadora.js
+++ calculadora.js
@@ -1,90 +1,91 @@
 var expressao = [];
+var expressaoParcial = [];
+var sequenciaOperacao = [];
 var botaoDigitado = "";
 var numero = "";
 var display = "";
 var digito = "";
 var display = "";
-var ultimoNumero ="";
+var ultimoNumero = "";
+var calculo = 0;
 
 
 //DIGITOS
 
 
 botaoDigitado = document.querySelector("#botao_9");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 9;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
-    
 });
 
 botaoDigitado = document.querySelector("#botao_8");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 8;
     guardaUltimoNumero();
     atualizaDisplay(digito), ultimoNumero;
-    
 });
 
 botaoDigitado = document.querySelector("#botao_7");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 7;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#botao_6");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 6;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
-    
+
 });
 
 botaoDigitado = document.querySelector("#botao_5");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 5;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#botao_4");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 4;
     guardaUltimoNumero();
-    atualizaDisplay(digito, ultimoNumero);  
+    atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#botao_3");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 3;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#botao_2");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 2;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#botao_1");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 1;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#zero");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = 0;
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
 });
 
 botaoDigitado = document.querySelector("#ponto");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = ".";
     atualizaDisplay(digito, ultimoNumero);
 });
@@ -94,15 +95,15 @@ botaoDigitado.addEventListener("click", function (){
 
 
 botaoDigitado = document.querySelector("#divisao");
-botaoDigitado.addEventListener("click", function (){
-    digito = "/";
-    guardaUltimoNumero();
-    atualizaDisplay(digito, ultimoNumero);
-    console.log(digito);
+botaoDigitado.addEventListener("click", function () {
+        digito = "/";
+        guardaUltimoNumero();
+        atualizaDisplay(digito, ultimoNumero);
+        console.log(digito);
 });
 
 botaoDigitado = document.querySelector("#multiplicacao");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = "*";
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
@@ -110,7 +111,7 @@ botaoDigitado.addEventListener("click", function (){
 });
 
 botaoDigitado = document.querySelector("#subtracao");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = "-";
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
@@ -118,7 +119,7 @@ botaoDigitado.addEventListener("click", function (){
 });
 
 botaoDigitado = document.querySelector("#soma");
-botaoDigitado.addEventListener("click", function (){
+botaoDigitado.addEventListener("click", function () {
     digito = "+";
     guardaUltimoNumero();
     atualizaDisplay(digito, ultimoNumero);
@@ -138,56 +139,61 @@ botaoDigitado.addEventListener("click", apagaUltimoDigito);
 //FUNÇÕES
 
 
-function atualizaExpressao(numero, operador){
-    expressao.push(numero, operador);
-    console.log(expressao);
+function atualizaExpressao(numero, operador) {
+
+    expressao.push(numero);
+    if (operador != "") {
+        expressao.push(operador);
+    }
+    sequenciaOperacao.push(operador);
 }
 
-function guardaUltimoNumero(){
+/*function atualizaExpressaoParcial(arrayexpressao, posicaoInicial, valorCalculoParcial){
+    arrayexpressao.splice(posicaoInicial,3,valorCalculoParcial);
+}*/
+
+function guardaUltimoNumero() {
     display = document.getElementById("display");
     ultimoNumero = display.textContent;
-    ultimoNumero = ultimoNumero.substring(ultimoNumero.length,ultimoNumero.length -1);
-    return ultimoNumero;
+    ultimoNumero = ultimoNumero.substring(ultimoNumero.length, ultimoNumero.length - 1);
+
 }
 
-function atualizaDisplay(valor, ultimoNumero){
+function atualizaDisplay(valor, ultimoNumero) {
+
+
     display = document.getElementById("display");
-    if (valor === "." &&   display.textContent.includes(".")) {
-            
-    }else if(valor === "/" &&  ultimoNumero ==="/"){
+    if (valor === "." && display.textContent.includes(".")) {
 
-    }else if(valor === "*" &&   ultimoNumero ==="*"){
+    } else if (valor === "/" && ultimoNumero === "/") {
 
-    }else if(valor === "+" &&   ultimoNumero ==="+"){
+    } else if (valor === "*" && ultimoNumero === "*") {
 
-    }else if(valor === "-" &&   ultimoNumero ==="-"){
+    } else if (valor === "+" && ultimoNumero === "+") {
 
-    }
-    
-    
-    else{
+    } else if (valor === "-" && ultimoNumero === "-") {
 
-            if(valor === "/" || valor === "*" || valor === "+" || valor === "-"){
-                atualizaExpressao(numero, valor);
-                numero = "";
-            }else{
-                numero = numero + valor;
+    } else {
+        console.log(numero);
+        console.log(valor);
+        if (valor === "/" || valor === "*" || valor === "+" || valor === "-") {
+            atualizaExpressao(numero, valor);
+            numero = "";
+        } else {
+            numero = numero + valor;
         }
 
         //display = document.getElementById("display");
         display.textContent = display.textContent + valor;
-        console.log(display);
-        console.log(numero);
-        console.log(valor);
 
     }
 }
 
-function limpaDisplay(){
+function limpaDisplay() {
     display = document.getElementById("display");
     display.textContent = "";
-    expressao= [];
     expressao = [];
+    sequenciaOperacao = [];
     botaoDigitado = "";
     numero = "";
     display = "";
@@ -197,12 +203,12 @@ function limpaDisplay(){
 
 }
 
-function apagaUltimoDigito(){
+function apagaUltimoDigito() {
     display = "";
     displayAtualizado = "";
     display = document.getElementById("display");
     displayAtualizado = display.textContent;
-    displayAtualizado = displayAtualizado.substring(0, displayAtualizado.length -1);
+    displayAtualizado = displayAtualizado.substring(0, displayAtualizado.length - 1);
     console.log(displayAtualizado);
     display.textContent = displayAtualizado;
     expressao.pop(digito);
@@ -210,87 +216,100 @@ function apagaUltimoDigito(){
 
 
 
-function calcula(){
+function calcula() {
+    expressao.push(numero);
+    console.log(expressao);
+    
+    var indice = 0;
 
+    //expressaoParcial = expressao
 
-    //let calculo = document.getElementById("display");
-    //calculo = calculo.textContent;
-    //expressao = calculo.split("");
+    for(indice = 0; expressao.length > indice; indice ++){
 
-    expressao.push(digito);
-
-    expressao.map(function(digito, indice){
-        if(digito === "*"){
-            
-            var posicaoValor1 = indice - 1; 
-            var posicaoValor2 = indice +1;
-
-            var valor1 = expressao[posicaoValor1];
-            var valor2 = expressao[posicaoValor2];
-            
-            var calculo = (parseFloat(valor1) * parseFloat(valor2));
-
-            console.log(digito);
-            console.log(posicaoValor1);
-            console.log(posicaoValor2);
-
-            console.log(valor1);
-            console.log(valor2);
-            console.log(calculo);
-
-            display.textContent = calculo;
-
-        }else if(digito === "/"){
-            
-            var posicaoValor1 = indice - 1; 
+        console.log(expressaoParcial);
+        
+        if (expressao[indice] === "*") {
+            console.log(expressao[indice]);
+            var posicaoValor1 = indice - 1;
             var posicaoValor2 = indice + 1;
 
             var valor1 = expressao[posicaoValor1];
             var valor2 = expressao[posicaoValor2];
-            
-            var calculo = (valor1 / valor2)
 
-            console.log(valor1);
-            console.log(valor2);
+            var calculo = (parseFloat(valor1) * parseFloat(valor2));
             console.log(calculo);
 
-            display.textContent = calculo;
+            //atualizaExpressaoParcial(expressao, posicaoValor1, calculo);
 
-        }else if(digito === "+"){
             
-            var posicaoValor1 = indice - 1; 
-            var posicaoValor2 = indice +1;
-
-            var valor1 = expressao[posicaoValor1];
-            var valor2 = expressao[posicaoValor2];
-            
-            var calculo = (parseFloat(valor1) + parseFloat(valor2));
-
-            console.log(valor1);
-            console.log(valor2);
-            console.log(calculo);
-
-            display.textContent = calculo;
-
-        }else if(digito === "-"){
-            
-            var posicaoValor1 = indice - 1; 
-            var posicaoValor2 = indice +1;
-
-
-
-            var valor1 = expressao[posicaoValor1];
-            var valor2 = expressao[posicaoValor2];
-            
-            var calculo = (valor1 - valor2)
-
-            console.log(valor1);
-            console.log(valor2);
-            console.log(calculo);
-
-            display.textContent = calculo;
-
+            expressao.splice(posicaoValor1,3,calculo);
+            console.log(expressao);
+            indice = 0
         }
         
-    })
+
+            
+    }
+
+
+        /*} else if (digito === "/") {
+            console.log(expressao);
+            var posicaoValor1 = indice - 1;
+            var posicaoValor2 = indice + 1;
+
+            var valor1 = expressao[posicaoValor1];
+            var valor2 = expressao[posicaoValor2];
+
+            var calculo = (parseFloat(valor1) / parseFloat(valor2));
+
+            exibeResultado(calculo);
+
+            console.log(calculo);
+            console.log(valor1);
+            console.log(valor2);
+            console.log(posicaoValor1);
+            console.log(posicaoValor2);
+
+        } else if (digito === "+") {
+
+            var posicaoValor1 = indice - 1;
+            var posicaoValor2 = indice + 1;
+
+            var valor1 = expressao[posicaoValor1];
+            var valor2 = expressao[posicaoValor2];
+
+            var calculo = (parseFloat(valor1) + parseFloat(valor2));
+
+            exibeResultado(calculo);
+
+        } else if (digito === "-") {
+
+            var posicaoValor1 = indice - 1;
+            var posicaoValor2 = indice + 1;
+
+
+
+            var valor1 = expressao[posicaoValor1];
+            var valor2 = expressao[posicaoValor2];
+
+            var calculo = (parseFloat(valor1) - parseFloat(valor2));
+
+            exibeResultado(calculo);
+        }
+    */
+
+
+
+}
+
+function resultadoParcial() {
+
+}
+
+function exibeResultado(resultado) {
+    console.log(resultado);
+    display.textContent = resultado;
+    expressao = [];
+    expressao = resultado;
+    console.log(expressao);
 }
